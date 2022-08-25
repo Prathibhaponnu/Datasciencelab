f2177ba144bd3b034e7c6d77de384007b4976d62
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-08-25 14:52:04 +0530

    Add files via upload

---
+++ coprime.py
@@ -0,0 +1,13 @@
+print("prathibha21mca034")
+import math
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+print("gcd of numbers")
+num3=math.gcd(num1,num2)
+print(num3)
+if(num3==1):
+    print("Given numbers are coprime")
+else:
+    print("Given numbers are not coprime")
+
+
