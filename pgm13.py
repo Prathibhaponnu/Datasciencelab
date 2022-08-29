ccdd24a69484ff4ec2171ff229d79443eca8f6de
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-08-29 11:35:06 +0530

    Add files via upload

---
+++ pgm13.py
@@ -0,0 +1,14 @@
+print("prathibha21mca034")
+num=int(input("Enter a number : "))
+def digitsum(num):
+    sum=0
+    while num>0:
+        rem=num%10;
+        sum=sum+rem;
+        num=num//10
+    return sum
+
+while(num>0):
+    sum=digitsum(num)
+    print("{} - {} = {}".format(num,sum,num-sum))
+    num=num-sum
