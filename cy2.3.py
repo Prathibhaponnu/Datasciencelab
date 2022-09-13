8da7e9a777fec24d4b11fa6019657532111dcd69
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-09-13 13:58:22 +0530

    Add files via upload

---
+++ cy2.3.py
@@ -0,0 +1,11 @@
+print("prathibha21mca034")
+import numpy as np
+empt_array=np.empty([2,3])
+print(empt_array)
+ones_array=np.ones([4,3])
+print(ones_array)
+zero_array=np.zeros([4,4])
+print(zero_array)
+
+
+
