5928a94a6126705bc0e409570f1e0400ee102d29
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-09-13 13:57:28 +0530

    Add files via upload

---
+++ 3d.py
@@ -0,0 +1,15 @@
+print("prathibha21mca034")
+import numpy as np
+a=np.array([[[10,20,30],[40,50,60],[70,80,90]],
+    [[15,25,35],[45,55,65],[75,85,95]],
+    [[13,23,33],[43,53,63],[73,83,93]]],dtype=float)
+print(a)
+print(a.ndim)
+print(a.shape)
+
+
+
+
+
+
+
