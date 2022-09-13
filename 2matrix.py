9e8d5eb4ef958b9f7441ffe33a06cc0f6019db1d
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-09-13 13:57:50 +0530

    Add files via upload

---
+++ 2matrix.py
@@ -0,0 +1,12 @@
+print("prathibha21mca034")
+import numpy as np
+a=np.array([[1,2,3],[4,5,6]],dtype=complex)
+print("Number of rows and columns:",a.shape)
+print("Dimension of an array:",a.ndim)
+print("Before reshape",a)
+print("After reshape",a.reshape(3,2))
+
+
+
+
+
