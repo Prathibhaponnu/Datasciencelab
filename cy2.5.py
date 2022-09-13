5c5b51b9112c821a680eaa16300c8c0b211c2b8d
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-09-13 13:58:52 +0530

    Add files via upload

---
+++ cy2.5.py
@@ -0,0 +1,14 @@
+print("prathibha21mca034")
+import numpy as np
+even_num=np.arange(0,15,2)
+print(even_num)
+print(even_num[2:8:2])
+print(even_num[-3:])
+print(even_num[::2])
+print(even_num[-6::2])
+
+
+
+
+
+
