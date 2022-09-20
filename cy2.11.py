1c0afc39a9229f81530c7c337651fedd9b9153aa
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-09-20 16:11:11 +0530

    Add files via upload

---
+++ cy2.11.py
@@ -0,0 +1,10 @@
+print("prathibha21mca034")
+import numpy as np
+a=np.array([10,20,30,40,50])
+print("sum is",np.sum(a))
+a1=np.array([[10,20,30],[40,50,60],[70,80,90]])
+print("sum ",np.sum(a1))
+print("sum of rows",sum(a1))
+
+
+
