c68e6bc085d4c0bacdc094ffe03bba545e309840
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-09-20 16:10:01 +0530

    Add files via upload

---
+++ cy2.8.py
@@ -0,0 +1,20 @@
+print("prathibha21mca034")
+import numpy as np
+a=np.array([1,2,3,4])
+print(a)
+print("-----------")
+a1=np.array([[5,6,7],[9,10,11],[1,2,3]])
+print(np.diag(a))
+print("............")
+print(a1)
+print(".............")
+print(np.diag(a1))
+print("..............")
+
+
+
+
+
+
+
+
