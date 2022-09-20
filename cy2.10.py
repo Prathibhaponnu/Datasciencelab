d9e175200311c40750b5ceb03d71405001127ca4
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-09-20 16:10:29 +0530

    Add files via upload

---
+++ cy2.10.py
@@ -0,0 +1,17 @@
+print("prathibha21mca034")
+import numpy as np
+a=np.array([1,2,3,4])
+a1=np.append(a,5)
+print(a)
+print("......")
+print(a1)
+print("......")
+a2=np.array([[10,20,30],[40,50,60],[70,80,90]])
+print(a2)
+print("......")
+a3=np.append(a2,(99,89,79))
+print(a3)
+
+
+
+
