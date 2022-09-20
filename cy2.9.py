d6739c7f3204e1cbc01ff83c988ad426a6e70d0e
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-09-20 16:10:13 +0530

    Add files via upload

---
+++ cy2.9.py
@@ -0,0 +1,14 @@
+print("prathibha21mca034")
+import numpy as np
+a=np.array([1,2,3,4,5])
+a2=np.array([[2,3,4],[5,6,7],[9,2,1]])
+a1=np.insert(a,2,6)
+a3=np.insert(a2,1,10,axis=1)
+print(a)
+print(a1)
+print(a2)
+print(a3)
+
+
+
+
