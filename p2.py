74178f8fd74f4ad132786a65857037cbd8a39311
Prathibhaponnu <100864129+Prathibhaponnu@users.noreply.github.com>
2022-10-11 12:18:54 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,22 @@
+print("prathibha21mca034")
+import numpy as np
+a=np.random.randint(10,size=(3,3))
+print(a)
+print("cube of each elements using power:")
+cube1=np.power(a,3)
+print(cube1)
+print("cube of each elements usimg multiply:")
+cube=np.multiply(a,a)
+cube2=np.multiply(cube,a)
+print(cube2)
+print("cube of each elements using *:")
+cube3=a*a*a
+print(cube3)
+print("cube of each elements using **:")
+cube4=a**3
+print(cube4)
+
+
+
+
+
